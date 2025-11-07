e7cff4e5f781af0eb012d046078e0264226cb266
Kubaryt <kubastarzyk6554@gmail.com>
2025-11-07 13:21:25 +0100

    merge sort uwu

--- merge_sort.py
+++ merge_sort.py
@@ -5,14 +5,26 @@ def merge_sort(array: list) -> list:
         first_fragment = merge_sort(array[:index_of_division])
         second_fragment = merge_sort(array[index_of_division:])
     else:
-        if array[0] > array[1]:
+        if length == 2 and array[0] > array[1]:
             return [array[1], array[0]]
         else:
             return array
 
     new_array = []
-    for i, n in enumerate(first_fragment):
-        while n > second_fragment[i]:
-            pass
+    for num in first_fragment:
+        while len(second_fragment) > 0 and num > second_fragment[0]:
+            new_array.append(second_fragment[0])
+            del second_fragment[0]
+        else:
+            new_array.append(num)
 
-    return []
+    if len(second_fragment) > 0:
+        new_array += second_fragment
+    return new_array
+
+
+if __name__ == "__main__":
+    print(merge_sort([5, 4, 3, 2, 1]))
+    print(merge_sort([10, 8, 9, 6, 7]))
+    print(merge_sort([21, 42, 37]))
+    print(merge_sort([2, 1, 3, 7, 4, 2, 0, 6, 9]))
